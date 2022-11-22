9844efe5bae0aee96f90ab7825a70adf789ad091
Barrocz <youberrody@gmail.com>
2022-11-22 12:31:51 +0000

    clean code

---
+++ Traffic1.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class TrafficAnalysis {
+
+
+
+
+
+
+
+
+	public static void main(String argv[]) throws Exception {
+
+	
+		while ((line = br.readLine()) != null) {
+			
+		} 
+
+		for (long size : sizes) {
+			
+		}
+		
+		
+
+	}// FIM DA MAIN
+
+	private static boolean isIPv4(String address) {
+		
+	}
+
+	private static boolean isIPv6(String address) {
+		
+	}
+}// FIM CLASSE
